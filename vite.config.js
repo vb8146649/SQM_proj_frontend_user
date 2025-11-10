4165b8498ad8fa9002bc180a3b0fa6b5bc87a37b
vb8146649 <vb8146649@gmail.com>
2025-11-10 19:33:11 +0530

    deleting the github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SQM_proj_frontend_user/',
+  // base: '/SQM_proj_frontend_user/',
 })
