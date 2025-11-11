73ef3d47280f03ba252c8d753d0506ffc512265f
vb8146649 <vb8146649@gmail.com>
2025-11-11 22:02:08 +0530

    update footer copyright year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,14 +32,14 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>Get in touch</h2>
           <ul>
-            <li>+92-308-4900522</li>
+            <li>+91-XXXXXXXXXX</li>
             <li>contact@tomato.com</li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 @ Tomato.com - All Right Reserved.
+        Copyright 2025 @ Tomato.com - All Right Reserved.
       </p>
     </div>
   );
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://sqm-proj-backend.onrender.com";
+  // const url = "https://sqm-proj-backend.onrender.com";
+  const url = "https://food-delivery-backend-5b6g.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
